cea0979f2c63d584a475c13fa9a8f98fbeb888c4
ChrisKim <1515748548@qq.com>
2025-04-14 21:57:36 +0800

    support my reasoner model

--- scripts/func_eval_gen.py
+++ scripts/func_eval_gen.py
@@ -6,8 +6,12 @@ import numpy
 import random
 import shutil
 import argparse
-from tqdm import tqdm
+import traceback
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
+
+from tqdm import tqdm
+from openai import OpenAI
 
 from safecoder.utils import set_seed, load_model
 from safecoder.human_eval.problem_yaml import Problem
@@ -22,7 +26,7 @@ def get_args():
     parser.add_argument('--temp', type=float, default=0.2)
     parser.add_argument('--top_p', type=float, default=0.95)
     parser.add_argument('--max_gen_len', type=int, default=256)
-    parser.add_argument('--num_samples', type=int, default=40)
+    parser.add_argument('--num-samples', type=int, default=40)
     parser.add_argument('--num_samples_per_gen', type=int, default=10)
 
     parser.add_argument('--experiments_dir', type=str, default='../experiments')
@@ -30,9 +34,20 @@ def get_args():
     parser.add_argument('--model_dir', type=str, default='../trained')
 
     parser.add_argument('--seed', type=int, default=1)
+
+    # MyResoner specific arguments
+    parser.add_argument('--use-my-reasoner', action='store_true')
+    parser.add_argument('--use-system-prompt', action='store_true')
+    parser.add_argument('--api-base', type=str, default='http://127.0.0.1:9997/v1')
+    parser.add_argument('--token', type=str, default='none')
+    parser.add_argument('--temperature', type=float, default=0.6)
+    parser.add_argument('--max-completion-tokens', type=int, default=16384)
+    parser.add_argument("--max-workers", type=int, default=8)
+
     args = parser.parse_args()
 
-    assert args.num_samples % args.num_samples_per_gen == 0
+    if not args.use_my_reasoner:
+        assert args.num_samples % args.num_samples_per_gen == 0
     args.output_dir = os.path.join(args.experiments_dir, args.eval_type)
     args.data_dir = os.path.join(args.data_dir, args.eval_type)
     os.makedirs(args.output_dir, exist_ok=True)
@@ -65,6 +80,58 @@ def trim_code(completion, stop_tokens):
             completion = completion[:completion.find(stop_token)]
     return completion
 
+def query_model(client, messages: list) -> tuple[str, str]:
+    completion = client.chat.completions.create(
+        model=args.model_name,
+        messages=messages,
+        temperature=args.temperature,
+        max_tokens=args.max_completion_tokens,
+        max_completion_tokens=args.max_completion_tokens,
+        stream=True
+    )
+
+    think_content = ""
+    answer_content = ""
+
+    for chunk in completion:
+        if not chunk.choices:
+            continue
+
+        delta = chunk.choices[0].delta
+        if hasattr(delta, "reasoning_content") and delta.reasoning_content != None:
+            think_content += delta.reasoning_content
+        elif hasattr(delta, "content") and delta.content != None:
+            answer_content += delta.content
+
+    return think_content, answer_content
+
+def extract_markdown(md):
+    pattern = r'```.*?\n(.*?)```'
+    matches = re.findall(pattern, md, re.DOTALL)
+    return matches
+
+def sample_one(client, messages: list) -> tuple[str, str] | None:
+    try:
+        _, answer_content = query_model(client, messages)
+    except Exception as e:
+        print(f"Error querying model: {e}")
+        traceback.print_exc()
+        return None
+
+    if answer_content == "":
+        print("Empty answer content")
+        return None
+
+    code_blocks = extract_markdown(answer_content)
+    if len(code_blocks) == 0:
+        print("No code blocks found")
+        return None
+
+    code_block = max(code_blocks, key=len)
+
+    return code_block
+
+
 def main():
     output_dir = Path(args.output_dir)
     if not output_dir.exists():
@@ -78,8 +145,19 @@ def main():
         )
     )
 
-    tokenizer, model = load_model(args.model_name, args)
-    model.eval()
+    if args.use_my_reasoner:
+        client = OpenAI(api_key=args.token, base_url=args.api_base)
+        system_prompt = """You are a helpful assistant. A conversation between User and Assistant. The user asks a question, and the Assistant solves it.
+The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
+The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively.
+At the same time, please wrap the generated code in markdown code blocks ``` ```.
+i.e., <think> reasoning process here </think><answer> answer here ``` code here ``` </answer>"""
+        user_prompt_prefix = "Completing the following function snippet, then output the full code in a ``` code block."
+
+    else:
+        tokenizer, model = load_model(args.model_name, args)
+        model.eval()
+
     is_pretrained = args.model_name in PRETRAINED_MODELS
     is_chat = args.model_name in CHAT_MODELS
 
@@ -87,7 +165,16 @@ def main():
         with problem_yaml_path.open() as f:
             problem = Problem.load(f)
         orig_prompt = problem.prompt.strip()
-        if is_chat:
+
+        # this is my reasoner specific
+        if args.use_my_reasoner:
+            messages = []
+            if args.use_system_prompt:
+                messages.append({'role': 'system', 'content': system_prompt})
+            
+            prompt = f"{user_prompt_prefix}\n\n```python\n{orig_prompt}\n```"
+            messages.append({'role': 'user', 'content': prompt})
+        elif is_chat:
             if args.model_name == 'octocoder':
                 template = 'Question: {instruction}\n\nAnswer: '
                 prompt = template.format_map({'instruction': INSTRUCTION.format_map({'language': 'Python', 'prompt': extract_docstr(orig_prompt)})})
@@ -105,42 +192,54 @@ def main():
         else:
             prompt = PROMPT_NO_INPUT.format_map({'instruction': INSTRUCTION.format_map({'language': 'Python', 'prompt': extract_docstr(orig_prompt)})})
             prompt += extract_funcsig(orig_prompt)
-        # print(prompt)
-        # print('='*150)
-        inputs = tokenizer(prompt.strip(), return_tensors='pt').to(model.device)
-        seed = args.seed
-        for i in range(args.num_samples // args.num_samples_per_gen):
-            set_seed(seed+i)
-            with torch.no_grad():
-                if hasattr(model.config, 'n_positions'):
-                    n_ctx = model.config.n_positions
-                elif hasattr(model.config, 'max_position_embeddings'):
-                    n_ctx = model.config.max_position_embeddings
-                else:
-                    n_ctx = 32000 # some arbitrary large context, risky as it could lead to errors
-                max_gen_len = max(0, min(n_ctx - 1 - len(inputs['input_ids'][0]), args.max_gen_len))
-                samples = model.generate(
-                    **inputs,
-                    do_sample=True,
-                    num_return_sequences=args.num_samples_per_gen,
-                    temperature=args.temp,
-                    max_new_tokens=max_gen_len,
-                    top_p=args.top_p,
-                    pad_token_id=tokenizer.eos_token_id,
-                    eos_token_id=tokenizer.eos_token_id,
-                    use_cache=True,
-                )
-            for sample in samples.tolist():
-                # print(tokenizer.decode(sample))
-                # print('*'*150)
-                completion = sample[inputs['input_ids'].shape[1]:]
-                if tokenizer.eos_token_id in completion:
-                    completion = completion[:completion.index(tokenizer.eos_token_id)]
-                completion = tokenizer.decode(completion)
-                completion = trim_code(completion, problem.stop_tokens)
-                # print(completion)
-                # print('='*150)
-                problem.completions.append(completion)
+
+        # this is my reasoner specific
+        if args.use_my_reasoner:
+            with ThreadPoolExecutor(max_workers=args.max_workers) as executor:
+                futures = [executor.submit(sample_one, client, messages) for _ in range(args.num_samples)]
+                with tqdm(total=len(futures), dynamic_ncols=True) as pbar:
+                    for future in as_completed(futures):
+                        res = future.result()
+                        if res is not None:
+                            problem.completions.append(res)
+                        pbar.update(1)
+        else:
+            # print(prompt)
+            # print('='*150)
+            inputs = tokenizer(prompt.strip(), return_tensors='pt').to(model.device)
+            seed = args.seed
+            for i in range(args.num_samples // args.num_samples_per_gen):
+                set_seed(seed+i)
+                with torch.no_grad():
+                    if hasattr(model.config, 'n_positions'):
+                        n_ctx = model.config.n_positions
+                    elif hasattr(model.config, 'max_position_embeddings'):
+                        n_ctx = model.config.max_position_embeddings
+                    else:
+                        n_ctx = 32000 # some arbitrary large context, risky as it could lead to errors
+                    max_gen_len = max(0, min(n_ctx - 1 - len(inputs['input_ids'][0]), args.max_gen_len))
+                    samples = model.generate(
+                        **inputs,
+                        do_sample=True,
+                        num_return_sequences=args.num_samples_per_gen,
+                        temperature=args.temp,
+                        max_new_tokens=max_gen_len,
+                        top_p=args.top_p,
+                        pad_token_id=tokenizer.eos_token_id,
+                        eos_token_id=tokenizer.eos_token_id,
+                        use_cache=True,
+                    )
+                for sample in samples.tolist():
+                    # print(tokenizer.decode(sample))
+                    # print('*'*150)
+                    completion = sample[inputs['input_ids'].shape[1]:]
+                    if tokenizer.eos_token_id in completion:
+                        completion = completion[:completion.index(tokenizer.eos_token_id)]
+                    completion = tokenizer.decode(completion)
+                    completion = trim_code(completion, problem.stop_tokens)
+                    # print(completion)
+                    # print('='*150)
+                    problem.completions.append(completion)
         with problem_yaml_path.open('w') as f:
             f.write(Problem.dump(problem))
 
